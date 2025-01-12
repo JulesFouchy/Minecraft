27e71ad03f85f37d96c378db013c02cf574a5fe4
Jules Fouchy <jules.fouchy@ntymail.com>
2025-01-12 21:18:55 +0100

    🔧 Improve run-web.py

--- run-web.py
+++ run-web.py
@@ -1,32 +1,8 @@
-import http.server
-import socketserver
 import os
 from pathlib import Path
-import webbrowser
 import shutil
 
 os.system("wasm-pack build --target web")
 shutil.copy("src-web/index.html", "pkg/index.html")
-
-
-class CustomHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
-    def end_headers(self):
-        # Set proper headers for modules
-        if self.path.endswith(".js"):
-            self.send_header("Content-Type", "application/javascript")
-        super().end_headers()
-
-
-# Configure the server
-PORT = 8000
-DIR = Path(__file__).parent / "pkg"  # Ensure this points to your 'pkg' directory
-
-import os
-
-os.chdir(DIR)
-
-# Set up the server with the custom handler
-Handler = CustomHTTPRequestHandler
-with socketserver.TCPServer(("", PORT), Handler) as httpd:
-    print(f"Serving {DIR} at http://localhost:{PORT}")
-    httpd.serve_forever()
+os.chdir(Path(__file__).parent / "pkg")
+os.system("http-server -o")
