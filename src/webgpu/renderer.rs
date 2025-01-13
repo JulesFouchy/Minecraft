90fd154e2fd24ce3b239ffca11b2dfc8389e35fa
Jules Fouchy <jules.fouchy@ntymail.com>
2025-01-13 01:46:45 +0100

    ✨ Cube mesh

--- src/webgpu/renderer.rs
+++ src/webgpu/renderer.rs
@@ -2,52 +2,30 @@ use super::*;
 use crate::camera::Camera;
 use wgpu::util::DeviceExt;
 
+#[rustfmt::skip]
 const VERTICES: &[Vertex] = &[
-    Vertex {
-        position: [-0.0868241, 0.49240386, 0.0],
-        uv: [0.4131759, 0.99240386],
-    }, // A
-    Vertex {
-        position: [-0.49513406, 0.06958647, 0.0],
-        uv: [0.0048659444, 0.56958647],
-    }, // B
-    Vertex {
-        position: [-0.21918549, -0.44939706, 0.0],
-        uv: [0.28081453, 0.05060294],
-    }, // C
-    Vertex {
-        position: [0.35966998, -0.3473291, 0.0],
-        uv: [0.85967, 0.1526709],
-    }, // D
-    Vertex {
-        position: [0.44147372, 0.2347359, 0.0],
-        uv: [0.9414737, 0.7347359],
-    }, // E
-];
-const VERTICES2: &[Vertex] = &[
-    Vertex {
-        position: [-0.0868241, 1. + 0.49240386, 0.0],
-        uv: [0.4131759, 0.99240386],
-    }, // A
-    Vertex {
-        position: [-0.49513406, 1. + 0.06958647, 0.0],
-        uv: [0.0048659444, 0.56958647],
-    }, // B
-    Vertex {
-        position: [-0.21918549, 1. + -0.44939706, 0.0],
-        uv: [0.28081453, 0.05060294],
-    }, // C
-    Vertex {
-        position: [0.35966998, 1. + -0.3473291, 0.0],
-        uv: [0.85967, 0.1526709],
-    }, // D
-    Vertex {
-        position: [0.44147372, 1. + 0.2347359, 0.0],
-        uv: [0.9414737, 0.7347359],
-    }, // E
+    // Front face
+    Vertex { position: [-0.5, -0.5,  0.5], uv: [0.0, 0.0] }, // 0
+    Vertex { position: [ 0.5, -0.5,  0.5], uv: [1.0, 0.0] }, // 1
+    Vertex { position: [ 0.5,  0.5,  0.5], uv: [1.0, 1.0] }, // 2
+    Vertex { position: [-0.5,  0.5,  0.5], uv: [0.0, 1.0] }, // 3
+
+    // Back face
+    Vertex { position: [-0.5, -0.5, -0.5], uv: [1.0, 0.0] }, // 4
+    Vertex { position: [ 0.5, -0.5, -0.5], uv: [0.0, 0.0] }, // 5
+    Vertex { position: [ 0.5,  0.5, -0.5], uv: [0.0, 1.0] }, // 6
+    Vertex { position: [-0.5,  0.5, -0.5], uv: [1.0, 1.0] }, // 7
 ];
 
-const INDICES: &[u16] = &[0, 1, 4, 1, 2, 4, 2, 3, 4];
+#[rustfmt::skip]
+const INDICES: &[u16] = &[
+    0, 1, 2, 0, 2, 3, // Front face
+    4, 5, 6, 4, 6, 7, // Back face
+    4, 0, 3, 4, 3, 7, // Left face
+    1, 5, 6, 1, 6, 2, // Right face
+    3, 2, 6, 3, 6, 7, // Top face
+    4, 5, 1, 4, 1, 0, // Bottom face
+];
 
 // We need this for Rust to store our data correctly for the shaders
 #[repr(C)]
@@ -70,7 +48,6 @@ impl CameraUniform {
 pub struct Renderer {
     render_pipeline: wgpu::RenderPipeline,
     cube_mesh: Mesh,
-    cube_mesh2: Mesh,
     diffuse_texture: Texture,
     texture_bind_group: wgpu::BindGroup,
     camera_buffer: wgpu::Buffer,
@@ -80,7 +57,6 @@ pub struct Renderer {
 impl Renderer {
     pub fn new(ctx: &Context, camera: &Camera) -> Self {
         let cube_model = Mesh::new(ctx, VERTICES, INDICES);
-        let cube_model2 = Mesh::new(ctx, VERTICES2, INDICES);
         let diffuse_texture = texture::Texture::from_bytes(
             ctx,
             include_bytes!("happy-tree.png"),
@@ -223,7 +199,6 @@ impl Renderer {
 
         Renderer {
             cube_mesh: cube_model,
-            cube_mesh2: cube_model2,
             diffuse_texture,
             render_pipeline,
             texture_bind_group: bind_group,
@@ -258,7 +233,6 @@ impl Renderer {
         render_pass.set_bind_group(1, &self.camera_bind_group, &[]);
 
         self.cube_mesh.draw(&mut render_pass);
-        self.cube_mesh2.draw(&mut render_pass);
     }
 
     pub fn set_camera(&self, ctx: &Context, camera: &Camera) {
